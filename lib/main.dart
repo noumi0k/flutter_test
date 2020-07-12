6b1e2fff34de0bec9a836dd1965e96c2835a1cd3
noumi0k <noumi0k@gmail.com>
2020-07-12 20:20:47 +0900

    click action

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test_app/Strings.dart';
 
 void main() {
-  runApp(SampleAppPage());
+  runApp(SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
 }
 
 class SampleAppPage extends StatefulWidget {
@@ -36,11 +49,22 @@ class _SampleAppPageState extends State<SampleAppPage> {
                 Text('Column Two'),
                 Text('Column Three'),
                 Text('Column Four'),
+                Center(
+                    child: GestureDetector(
+                  child: FlutterLogo(
+                    size: 200.0,
+                  ),
+                  onTap: () {
+                    print("tap");
+                  },
+                )),
+                RaisedButton(
+                    onPressed: () {
+                      print("click");
+                    },
+                    child: Text("Button")),
               ],
             ),
-            Text('Row Two'),
-            Text('Row Three'),
-            Text('Row Four'),
           ],
         ));
   }
